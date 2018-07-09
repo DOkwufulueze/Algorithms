ebed0f02e52296e9dbb7359bf60e993fd6ae775d
Daniel Okwufulueze <daniel.okwufulueze@gmail.com>
2018-07-09 01:42:07 +0100

    alg-4: Add comments to Multiply algorithm.

--- Multiply.js
+++ Multiply.js
@@ -1,4 +1,9 @@
-
+/*
+* Author: Daniel Okwufulueze
+* Date: 09/07/2018
+* Name: Multiply
+* Purpose: Multiply numbers using the carry method with an array data store. This method is efficient for computing large multiplications [eg. factorial of 500 -- 500! whose value is very well beyond the capacity of a 64-bit machine to store] and not having memory-limit issues when storing the result of the mutiplication.
+*/
 
 function multiply (a, b) {
   let aArray = a.toString().split("").reverse(); // Bring the lsd to the left
